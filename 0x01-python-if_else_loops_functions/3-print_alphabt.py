62c2092f3ca077d0cd9ad283f2067a557a506d40
Johnny <johnametepeagboku@live.com>
2023-10-03 14:46:49 +0000

    Python 2: Initial Commit

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,2 @@
+#!/usr/bin/python3
+print(''.join(chr (i) for i in range(97, 123) if chr(i) != 'e' and chr(i) != 'q'))
---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,2 @@
+#!/usr/bin/python3
+print(''.join('0' + str(i) + ', ' if i < 10 else str(i) + ', ' if i < 99 else str(i) + '\n' for i in range(0, 100)), end="")
