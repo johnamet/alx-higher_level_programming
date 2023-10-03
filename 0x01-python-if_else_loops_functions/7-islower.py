c227534f3536dd483e6d4448bc5e7d0d3d5b8260
Johnny <johnametepeagboku@live.com>
2023-10-03 15:58:53 +0000

    Fixup: Task 7

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def islower(c):
-    is_lower = ord(c) >= 97 and ord(c) < 123
-
-    if (is_lower):
-        print("{} is lower".format(c))
-    else:
-        print("{} is upper".format(c))
     return ord(c) >= 97 and ord(c) < 123
