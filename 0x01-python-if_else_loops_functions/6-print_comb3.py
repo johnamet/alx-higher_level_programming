763c395e2f680a036d6de94a3bbbea1f8d8cd59e
Johnny <johnametepeagboku@live.com>
2023-10-03 15:48:01 +0000

    Fixup: Coding style

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-print(*['{:02d}'.format(i) for i in range(10) for j in range(i+1, 10)], sep=', ')
+print(*['{:02d}'.format(i) for i in range(10)
+        for j in range(i+1, 10)], sep=', ')
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
     str = "{}".format(''.join(chr(ord(i))
-                  if ord(i) < 97 else chr(ord(i) - 32)
-                  for i in str))
+                              if ord(i) < 97 else chr(ord(i) - 32)
+                              for i in str))
     return str
