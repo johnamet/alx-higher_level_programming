2ee1b49df91ee7027eada0085c98fb18d241649d
Johnny <johnametepeagboku@live.com>
2023-10-06 08:48:32 +0000

    Fixup: Task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
+from add_0 import add
 # Define the values of a and b
 a = 1
 b = 2
-
-# Import the add function from add_0.py
-from add_0 import add
-
 # Calculate the result
 result = add(a, b)
 
@@ -13,4 +10,3 @@ result = add(a, b)
 if __name__ == "__main__":
     # Any additional code you want to run when the script is executed directly
     print("{} + {} = {}".format(a, b, result))
-
