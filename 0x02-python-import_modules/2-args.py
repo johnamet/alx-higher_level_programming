486efe72c95567107417a024359d12ec459fc963
Johnny <johnametepeagboku@live.com>
2023-10-06 21:47:45 +0000

    Fixup: Task2 coding style

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 import sys
+
+
 def main(*args):
-    
     if len(args) == 1:
         print("1 argument:")
     else:
-        print("{} arguments{}".format(len(args), "." if len(args) == 0 else ":"))
-
+        print("{} arguments{}".format(len(args),
+                                      "." if len(args) == 0 else ":"))
     if len(args) != 0:
         for i in range(len(args)):
             print("{}: {}".format(i+1, args[i]))
+
 
 if __name__ == "__main__":
     args = sys.argv[1:]
