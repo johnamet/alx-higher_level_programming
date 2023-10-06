1e0485cf9e0f89274c8e94d05347f46e888d7b39
Johnny <johnametepeagboku@live.com>
2023-10-06 11:06:57 +0000

    Fixup: Task 1: My first toolbox

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,6 +4,7 @@ from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
 
+if __name__ == "__main__":
 print("{a} + {b} = {add}\n{a} - {b} = {sub}"
       .format(a=a, b=b, add=add(a, b), sub=sub(a, b)))
 print("{a} * {b} = {mul}\n{a} / {b} = {div}"
