95d73340e2948d16a50eda4e817d65420c972d1c
Johnny <johnametepeagboku@live.com>
2023-10-06 11:05:30 +0000

    Task 1: My first toolbox

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+
+a = 10
+b = 5
+
+print("{a} + {b} = {add}\n{a} - {b} = {sub}"
+      .format(a=a, b=b, add=add(a, b), sub=sub(a, b)))
+print("{a} * {b} = {mul}\n{a} / {b} = {div}"
+      .format(a=a, b=b, mul=mul(a, b), div=div(a, b)))
