a9b5e4491a3a18a75aeaed2fb8c058ae221c6d48
Johnny <johnametepeagboku@live.com>
2023-10-07 20:03:31 +0000

    Task 2: Replace element

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+
+    size = len(my_list)
+
+    if idx > 0 and idx <= size - 1:
+        my_list[idx] = element
+
+    return my_list
