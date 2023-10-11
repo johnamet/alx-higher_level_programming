de3330158239eafc7c3cf49a51c77cc61dce733b
Johnny <johnametepeagboku@live.com>
2023-10-11 06:10:02 +0000

    Fixup: Task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    if len(my_list) != 0 or my_list is not None:
+    if my_list is not None or len(my_list) != 0:
         my_list.reverse()
 
         for i in my_list:
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,5 @@
 def print_matrix_integer(matrix=[[]]):
 
     for vector in matrix:
-        v_size = len(vector)
         for j in vector:
             print("{:d}".format(j, " " if vector[-1] != j else ""), end="")
