b53277269ca1a8e519f622d2121371596fc95174
Johnny <johnametepeagboku@live.com>
2023-10-24 11:19:23 +0000

    Fixup: Task 1: Handle TypeErrors

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -4,10 +4,7 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
     except TypeError:
-        try:
-            print("{:d}".format(value[0]))
-        except TypeError:
-            return False
+        return False
     except ValueError:
         return False
 
