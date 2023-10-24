abd7dd49989c184d6dff0add16c5e3f381fd2895
Johnny <johnametepeagboku@live.com>
2023-10-24 11:10:13 +0000

    Task 1- Safe print Integer

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+    except ValueError:
+        try:
+            value = int(value)
+        except ValueError:
+            return False
+
+    return True
