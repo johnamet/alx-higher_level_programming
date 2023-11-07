85689efda0bbd0dc364fb52c3a49f5e7244ca892
Johnny <johnametepeagboku@live.com>
2023-11-07 17:28:13 +0000

    Task 4: Inherits from

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""The module contains one function inherits from
+    that checks if an obj is a subclass of a class
+"""
+
+
+def inherits_from(obj, a_class):
+    """The function checks if an objects is a subclass
+        of a class.
+    Args:
+        obj (obj): The object
+        a_class (cls): the class
+    Return:
+        True if the object is a subclass otherwise False
+    """
+
+    return issubclass(obj, a_class)
