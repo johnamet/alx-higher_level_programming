69d817ef8c57f5208471a9516fd51b5a95beed63
Johnny <johnametepeagboku@live.com>
2023-11-07 16:20:47 +0000

    Task 2: is same class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,17 @@
+"""This module contains one function `is_same_class`
+    it checks whether an object is exactly an instance
+    of a class
+"""
+
+def is_same_class(obj, a_class):
+    """The function checks if an object is an instance
+        of a class.
+        Args:
+            obj (:object): The object
+            a_class (object): The class
+
+        Returns:
+            :bool - True if it is an instance otherwise False
+    """
+
+    return isinstance(obj, a_class)
