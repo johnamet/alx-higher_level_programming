cc70fece3a96aef6b5278ed2324e788b01c84444
Johnny <johnametepeagboku@live.com>
2023-11-07 15:14:35 +0000

    Task 0: Lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""The module contains onr function that prints the 
+    attributes and methods of a class
+"""
+
+
+def lookup(obj):
+    """The function returns the list of available attributes 
+        and methods of an object.
+    Args:
+        obj (:object): the object to list attributes
+
+    Returns:
+        The attributes and methods of an object
+    """
+
+    return dir(obj)
