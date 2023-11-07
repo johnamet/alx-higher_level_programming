4441bf52ccdc5d024a59f3d562751b4675ffd0dc
Johnny <johnametepeagboku@live.com>
2023-11-07 16:00:22 +0000

    Task 1: Print sorted

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""The module contains MyList class that inherits from
+    the list object
+"""
+
+class MyList(list):
+    """MyList class inherits from the list object
+        Args:
+        Returns:
+    """
+    def __init__(self):
+        super().__init__(self)
+
+    def print_sorted(self):
+        """Print sorted prints a sorted list"""
+
+        if any(not isinstance(item, int) for item in self):
+            raise TypeError("element needs to be an int")
+        
+        print(sorted(self))
