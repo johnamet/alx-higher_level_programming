0618b38194cd150b21dfa7c5c3cbda57ea9fefdb
Johnny <johnametepeagboku@live.com>
2023-11-15 11:40:33 +0000

    Fixup: exception for y if value is non-positive

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -76,7 +76,7 @@ class Rectangle(Base):
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
         elif value < 0:
-            raise ValueError("x must be >= 0")
+            raise ValueError("y must be >= 0")
         else:
             self.__y = value
 
