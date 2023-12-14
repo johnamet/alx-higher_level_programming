84b92bb456911f7a46f558b1dfb9982da8440478
Johnny <johnametepeagboku@live.com>
2023-12-14 13:54:27 -0800

    Task 102

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,15 @@
+-- temperature
+
+USE hbtn_0c_0;
+
+source temperatures.sql;
+
+SELECT
+    city,
+    AVG(value * 9/5 + 32) AS avg_temperature_fahrenheit
+FROM
+    temperatures
+GROUP BY
+    city
+ORDER BY
+    avg_temperature_fahrenheit DESC;
