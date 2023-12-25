9ef5085eb009abaa0c5a83e2a1b68ffdcb55b8c4
johnny <johnametepeagboku@live.com>
2023-12-25 15:18:46 +0000

    Task 11

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,7 @@
+--  lists all shows contained in the database hbtn_0d_tvshows.
+
+SELECT s.title, COALESCE(g.genre_id, 'NULL') AS genre_id
+FROM tv_shows AS s
+LEFT JOIN tv_show_genres AS g ON s.id = g.show_id
+ORDER BY s.title ASC, g.genre_id ASC;
+
