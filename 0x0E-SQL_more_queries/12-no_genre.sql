0d51d7e8dc487ac1aba6ac9916a2ab9c1444e473
johnny <johnametepeagboku@live.com>
2023-12-25 23:47:01 +0000

    Fixup: Task 12

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,8 +1,7 @@
---  lists all shows contained in the database hbtn_0d_tvshows.
+-- list all tv shows without genre
 
-SELECT s.title, COALESCE(g.genre_id, 'NULL') AS genre_id
-FROM tv_shows AS s
-LEFT JOIN tv_show_genres AS g ON s.id = g.show_id
-WHERE genre_id = 'NULL'
-ORDER BY s.title ASC, g.genre_id ASC;
-
+SELECT tv_shows.title, COALESCE(tv_show_genres.genre_id, 'NULL') genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
