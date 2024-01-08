af3b5fbdee9cc3ea3bdd077942fafe6dbeca1e1d
johnny <johnametepeagboku@live.com>
2024-01-08 22:17:57 +0000

    Fixup: Singular and Plural

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -5,6 +5,8 @@ const passedArgs = argv.slice(2);
 
 if (passedArgs.length === 0) {
   console.log('No argument');
+} else if (passedArgs.length === 1) {
+  console.log('Argument found');
 } else {
   console.log('Arguments found');
 }
