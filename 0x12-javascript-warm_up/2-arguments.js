20e140c7f671b1579df12e6551a14c14d057109e
johnny <johnametepeagboku@live.com>
2024-01-08 22:15:14 +0000

    Fixup: Task2- Coding style

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-const {argv } = require('node:process');
+const { argv } = require('node:process');
 
-let passed_args = argv.slice(2);
+const passedArgs = argv.slice(2);
 
-if (passed_args.length === 0)
-{
-	console.log('No argument');
-}else{
-	console.log('Arguments found');
+if (passedArgs.length === 0) {
+  console.log('No argument');
+} else {
+  console.log('Arguments found');
 }
