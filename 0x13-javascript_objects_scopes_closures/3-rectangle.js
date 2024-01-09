03e27d01ea8da30c21b78c5a2279efa31b5b39d2
johnny <johnametepeagboku@live.com>
2024-01-09 22:52:02 +0000

    Task 4

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -3,7 +3,7 @@ class Rectangle {
   constructor (w, h) {
     if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number'
         || !Number.isInteger(w) || !Number.isInteger(h)) {
-    
+      return this;    
     } else {
     this.width = w;
     this.height = h;
