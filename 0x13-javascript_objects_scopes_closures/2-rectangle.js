dea994e187be077bd56b4ad1281914ec82949787
johnny <johnametepeagboku@live.com>
2024-01-09 22:28:13 +0000

    Task 2

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+class Rectangle {
+  width;
+  height;
+
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number'
+        || !Number.isInteger(w) || !Number.isInteger(h)) {
+    
+    } else {
+    this.width = w;
+    this.height = h;
+    }
+  }
+}
+
+module.exports = Rectangle;
