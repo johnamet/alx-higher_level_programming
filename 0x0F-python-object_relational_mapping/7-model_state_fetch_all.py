c2506aa195a358ba5f5fc176552e3bc344c414fa
johnny <johnametepeagboku@live.com>
2024-01-13 19:52:17 +0000

    Fixup

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -13,8 +13,7 @@ if __name__ == "__main__":
     password = sys.argv[2]
     database = sys.argv[3]
 
-    engine = create_engine('mysql+mysqlconnector\
-            ://{}:{}@localhost:3306/{}'
+    engine = create_engine('mysql+mysqlconnector://{}:{}@localhost:3306/{}'\
                            .format(username, password, database))
     Base.metadata.create_all(engine)
 
