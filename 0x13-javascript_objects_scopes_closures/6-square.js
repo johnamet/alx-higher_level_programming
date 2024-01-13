dc9221c21fd0a2e498298a7652594730be9ebec2
johnny <johnametepeagboku@live.com>
2024-01-13 14:34:05 +0000

    Task 0

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,11 +2,14 @@
 const Sq = require('./5-square');
 class Square extends Sq {
   constructor (size) {
-    super(size);
+    super(size, size);
     this.size = size;
   }
 
-  charPrint (c = 'X') {
+  charPrint (c) {
+    if (c == undefined) {
+      c = 'X';
+    }
     for (let i = 0; i < this.size; i++) {
       let row = '';
       for (let j = 0; j < this.size; j++) {
