ee8e9eff677debcf880bdb0009afcada63b6fc24
johnny <johnametepeagboku@live.com>
2024-01-13 17:02:30 +0000

    Task 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Lista all states with a name starting matching with passed arg
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    passwd = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(host="localhost",
+                         user=username,
+                         passwd=passwd,
+                         port=3306,
+                         database=database)
+
+    cursor = db.cursor()
+    query = """SELECT cities.id, cities.name, states.name\
+            FROM cities, states\
+            ORDER BY cities.id ASC """
+
+    cursor.execute(query)
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
