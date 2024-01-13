6d277739045d5547232064f11ee09509d9d96796
johnny <johnametepeagboku@live.com>
2024-01-13 17:16:30 +0000

    Task

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Lista all cities with a name starting matching with passed arg
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    passwd = sys.argv[2]
+    database = sys.argv[3]
+    city_name = sys.argv[4]
+
+    db = MySQLdb.connect(host="localhost",
+                         user=username,
+                         passwd=passwd,
+                         port=3306,
+                         database=database)
+
+    cursor = db.cursor()
+    query = """
+            SELECT cities.id, cities.name, states.name
+            FROM cities
+            JOIN states ON cities.state_id = states.id
+            WHERE cities.name = %s
+            ORDER BY cities.id ASC
+            """
+
+    cursor.execute(query, (city_name,))
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
