bf8775daa13c17a425dc586a165059b6e292de8d
johnny <johnametepeagboku@live.com>
2024-01-14 14:39:04 +0000

    Task 14

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+The script adds the State object Louisiana to the database
+"""
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import create_engine, text
+import sys
+
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:\
+                           {}@localhost:3306/{}'
+                           .format(user, passwd, database))
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    rows = session.query(City).order_by(City.id).all()
+
+    for row in rows:
+        print("{}: ({}) {}".format(row.state_id.name,
+                                   row.id, row.name))
