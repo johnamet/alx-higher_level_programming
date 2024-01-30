a13a522ef8859489d39c901ff2cca62f12524597
johnny <johnametepeagboku@gmail.com>
2024-01-30 08:50:56 +0000

    Fixup task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Takes URL sends a GET requst and displays the body of the response
-if [ $(curl -s -o /dev/null -w "%{http_code}" "$1") -eq 200 ]; then curl -s "$1"; fi;
-
+curl -s -f "$1"
