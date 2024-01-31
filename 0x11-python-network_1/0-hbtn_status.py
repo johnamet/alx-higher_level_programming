6cae31961661cac0e859a67aedf3cc7667637091
johnny <johnametepeagboku@gmail.com>
2024-01-31 02:39:16 +0000

    Fixup

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,10 +4,11 @@ The script fetches  https://alx-intranet.hbtn.io/status
 """
 import urllib.request
 
-
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io"
-    with urllib.request.urlopen(url) as response:
+    req = urllib.request.Request(url)
+
+    with urllib.request.urlopen(req) as response:
         body = response.read()
         print("Body response:")
         print("\t- type:", type(body))
