f57be3ba8aa77c02045902de5875a41093b3b28f
johnny <johnametepeagboku@gmail.com>
2024-03-02 18:32:09 +0000

    Task 2: Post email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,11 +9,27 @@ import sys
 import urllib.parse
 import urllib.request
 
+
+def post_email(email, url):
+    """
+    Sends a POST request to the passed URL with the email as a parameter
+    :param email: The email address passed
+    :param url: The url to post the email to
+    :return: The body of the response
+    """
+    values = {'email': email}
+    headers = {'User-Agent': 'Mozilla/5'}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data, headers)
+
+    with urllib.request.urlopen(req) as response:
+        return response.read()
+
+
 if __name__ == "__main__":
     url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(value).encode("ascii")
+    email = sys.argv[2]
+    res = post_email(email, url)
 
-    request = urllib.request.Request(url, data)
-    with urllib.request.urlopen(request) as response:
-        print(response.read().decode("utf-8"))
+    print(res)
