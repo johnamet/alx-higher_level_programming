485262ea51c96cc113101856d9df0da71e0df210
johnny <johnametepeagboku@gmail.com>
2024-03-02 18:40:56 +0000

    Task 2: Post email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
 A script that takes in a URL and an email, sends a POST request to the URL
-with the email as a parameter, and displays the decoded UTF-8 body of the response.
+with the email as a parameter, and displays the
+decoded UTF-8 body of the response.
 """
 import sys
 import urllib.parse
@@ -10,7 +11,8 @@ import urllib.request
 
 def post_email(email, url):
     """
-    Sends a POST request to the URL with the email as a parameter in JSON format.
+    Sends a POST request to the URL with the email
+    as a parameter in JSON format.
 
     Args:
         email (str): The email address to be sent.
@@ -24,7 +26,8 @@ def post_email(email, url):
     try:
         # Build data as JSON for better compatibility with servers
         data = ({'email': email}.encode('utf-8'))
-        headers = {'Content-Type': 'application/json', 'User-Agent': 'Mozilla/5'}
+        headers = {'Content-Type': 'application/json',
+                   'User-Agent': 'Mozilla/5'}
 
         # Create and send the request using a with statement
         req = urllib.request.Request(url, data, headers)
@@ -46,6 +49,7 @@ if __name__ == "__main__":
         else:
             print("Error occurred during request.")
     except IndexError:
-        print("Error: Missing arguments. Please provide a URL and an email address.")
+        print("Error: Missing arguments."
+              "Please provide a URL and an email address.")
     except Exception as e:
         print(f"Unexpected error: {e}")
