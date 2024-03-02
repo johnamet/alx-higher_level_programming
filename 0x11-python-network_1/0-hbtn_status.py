546148c467c4d11b72122daac2f1d6324426e0f6
johnny <johnametepeagboku@gmail.com>
2024-03-02 18:03:44 +0000

    Tas 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,12 +5,12 @@ The script fetches  https://alx-intranet.hbtn.io/status
 import urllib.request
 
 if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io"
+    url = "https://alx-intranet.hbtn.io/status"
     req = urllib.request.Request(url)
 
     with urllib.request.urlopen(req) as response:
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
-        print("\t- content:".format(body))
-        print("\t- utf8 content: ".format(body.decode("utf-8")))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
