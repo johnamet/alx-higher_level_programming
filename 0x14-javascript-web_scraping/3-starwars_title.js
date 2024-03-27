8a9e7077c3b1109fe49bf4fee8388dca9db50450
johnny <johnametepeagboku@gmail.com>
2024-03-27 21:40:33 +0000

    Fixup: Task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -11,13 +11,9 @@ const episode = process.argv[2];
 //The url
 const url = `https://swapi-api.alx-tools.com/api/films/${episode}`;
 
-request.get(url).on('response', async function(err, response, body) {
-	if (err) {
-		console.log('Error', err);
-		return;
-	}
-	console.log(response);
-	console.log(body);
-	const movie = JSON.parse(body);
-	console.log(movie.title);
-});
+const request = new Request(url);
+
+const response = await fetch(request);
+const movie = await response.json();
+
+console.log(movie.title);
