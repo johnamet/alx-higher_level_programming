9f05627fd4db905af6e850a0163cd1499e9f0b53
johnny <johnametepeagboku@gmail.com>
2024-03-27 22:46:43 +0000

    Task4: Star wars count

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+/**
+ * Prints the title of Star Wars movie
+ * where the episode number matches a given integer
+ */
+const request = require('request');
+
+// Get the episode number from command-line arguments
+const episode = process.argv[2];
+
+// The URL
+const url = `https://swapi-api.alx-tools.com/api/films/`;
+
+// Send a GET request to the Star Wars API
+request.get(url, function (error, response, body) {
+  if (error) {
+    return;
+  }
+  // Parse the response body as JSON
+  const movieJson = JSON.parse(body);
+  const results = movieJson.results;
+
+  const filterd = results.map(movie => movie.characters.filter(url => url.endsWith("18")))
+  .filter(movie => movie.characters.length === 0)
+
+  console.log(filterd)
+});
