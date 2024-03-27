46055047b73d80574e8e5707f1246802ceda7495
johnny <johnametepeagboku@gmail.com>
2024-03-27 20:55:52 +0000

    Task 2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/**
+ * The script displays the status code of a GET request
+ */
+const request = require('request');
+
+// Get the url
+const url = process.argv[2];
+
+// send a get request
+request.get(url).on('response', function(response) {
+	console.log(`code: ${response.statusCode}`)
+});
